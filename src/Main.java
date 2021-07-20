025673140c22bf3979c241b054715d6c80461b6d
Mikko Salomäki <ms@datarespons.se>
2021-07-20 23:31:56 +0200

    Main use Parser

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,33 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		Dog.Builder b = new Dog.Builder(); 
-		System.out.println("Name?\n");
-		b.withName(s.nextLine());
-		System.out.println("Breed?\n");
-		b.withBreed(s.nextLine());
-		System.out.println("Age?\n");
-		String tmpAge = s.nextLine();
-		b.withAgeYears(Integer.parseUnsignedInt(tmpAge));
-		System.out.println("Weight?\n");
-		String tmpWeight = s.nextLine();
-		b.withWeightKg(Integer.parseUnsignedInt(tmpWeight));
-		Dog dog = b.build();
+		final String input = "Kalle\nGolden\n5\n10\n";
+		Scanner s = new Scanner(input);
+		//Scanner s = new Scanner(System.in);
+		Parser parser = new Parser();
+		while (!parser.done()) {
+			switch (parser.getField()) {
+			case NAME:
+				System.out.println("Name?\n");
+				break;
+			case BREED:
+				System.out.println("Breed?\n");
+				break;
+			case AGE_YEARS:
+				System.out.println("Age?\n");
+				break;
+			case WEIGHT_KG:
+				System.out.println("Weight?\n");
+				break;
+			}
+			
+			String str = s.nextLine();
+			Parser.Error r = parser.parseNext(str);
+			if (r != Parser.Error.OK) {
+				System.out.println("Error: " + r.name());
+			}			
+		}
+		Dog dog = parser.build();
 		System.out.println("name: " + dog.name +": breed: " + dog.breed + ": age: " + Integer.toString(dog.age_years) + ": weight: "
 								+ Integer.toString(dog.weight_kg));
 	}
